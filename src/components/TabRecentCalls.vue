77fab47fcd4379dd6757217a2559809de2fd3117
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-03-04 18:45:52 +0200

    Fixed: template TabRecentCalls.vue

--- src/components/TabRecentCalls.vue
+++ src/components/TabRecentCalls.vue
@@ -10,19 +10,25 @@ export default {
 </script>
 
 <template>
-  <div class="recent-call col s12">
-    <ul id="app-recent-calls" class="collection">
-      <div id="tab-2" class="col s12">
-        <div class="container">
-          <div class="row valign-wrapper">
-            <RecentCallItem
-              v-for="contact in contacts"
-              :key="contact.id"
-              :contact="contact"
-            />
-          </div>
+  <div id="tab-2" class="col s12" style="display: none">
+    <div class="container">
+      <div class="row valign-wrapper">
+        <div class="recent-call col s12">
+          <ul id="app-recent-calls" class="collection">
+            <div id="tab-2" class="col s12">
+              <div class="container">
+                <div class="row valign-wrapper">
+                  <RecentCallItem
+                    v-for="contact in contacts"
+                    :key="contact.id"
+                    :contact="contact"
+                  />
+                </div>
+              </div>
+            </div>
+          </ul>
         </div>
       </div>
-    </ul>
+    </div>
   </div>
 </template>
