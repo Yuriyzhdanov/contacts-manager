61b0696323cf50d5c72bc1754e4fdd752e3ce771
Yuriyzhdanov <yuuriyzhdanov@gmail.com>
2025-03-10 16:07:39 +0200

    added checks for the function findContactsByQuery

--- src/findContact.js
+++ src/findContact.js
@@ -1,27 +1,18 @@
-const contacts = [
-  { firstName: 'Вася', lastName: 'Петров', phone: '54321' },
-  { firstName: 'Тома', lastName: 'Ветров', phone: '5821' },
-  { firstName: 'Ирина', lastName: 'Васильева', phone: '3456' },
-  { firstName: 'Ирина', lastName: 'Иринова', phone: '1234' },
-  { firstName: 'Ирина', lastName: 'Ириновна', phone: '1234' },
-  { firstName: 'Ирина', lastName: 'Ириновна', phone: '12345' },
-]
-
-function findContactsByQuery(q) {
-  q = q.toLocaleLowerCase().trim()
+  q = q ? q.toLowerCase().trim() : ''
 
   if (q === '') return []
 
-  const queries = q.split(' ').filter(q => q)
+  const queries = q.split(' ').filter(Boolean)
 
   const contactMatches = contacts.map(contact => {
     let matchCount = 0
 
     queries.forEach(query => {
       if (
-        contact.phone.toLocaleLowerCase().includes(query) ||
-        contact.firstName.toLocaleLowerCase().includes(query) ||
-        contact.lastName.toLocaleLowerCase().includes(query)
+        (contact.phone && contact.phone.toLowerCase().includes(query)) ||
+        (contact.firstName &&
+          contact.firstName.toLowerCase().includes(query)) ||
+        (contact.lastName && contact.lastName.toLowerCase().includes(query))
       ) {
         matchCount++
       }
@@ -31,7 +22,7 @@ function findContactsByQuery(q) {
     return { contact, matchCount }
   })
 
-  const maxMatches = Math.max(...contactMatches.map(item => item.matchCount))
+  const maxMatches = Math.max(...contactMatches.map(item => item.matchCount), 0)
 
   return contactMatches
     .filter(item => item.matchCount === maxMatches && item.matchCount > 0)
