9daf03c3c69cdfd66ce51ec26bd3bec7564d6a9b
Yuriyzhdanov <yuuriyzhdanov@gmail.com>
2025-03-10 15:43:15 +0200

    solution #2 for sort function that returns maximum matches(incorrect work)

--- src/findContact.js
+++ src/findContact.js
@@ -1,58 +1,41 @@
 const contacts = [
   { firstName: 'Вася', lastName: 'Петров', phone: '54321' },
-  { firstName: 'Тома', lastName: 'Ретров', phone: '5821' },
+  { firstName: 'Тома', lastName: 'Ветров', phone: '5821' },
   { firstName: 'Ирина', lastName: 'Васильева', phone: '3456' },
   { firstName: 'Ирина', lastName: 'Иринова', phone: '1234' },
   { firstName: 'Ирина', lastName: 'Ириновна', phone: '1234' },
+  { firstName: 'Ирина', lastName: 'Ириновна', phone: '12345' },
 ]
 
 function findContactsByQuery(q) {
-  const findedContacts = []
-
   q = q.toLocaleLowerCase().trim()
 
   if (q === '') return []
 
   const queries = q.split(' ').filter(q => q)
 
-  contacts.forEach(contact => {
+  const contactMatches = contacts.map(contact => {
     let matchCount = 0
-    let bestMatch = ''
-    let matchMap = {}
 
     queries.forEach(query => {
-      let count = 0
-
-      if (contact.phone.toLocaleLowerCase().includes(query)) count++
-      if (contact.firstName.toLocaleLowerCase().includes(query)) count++
-      if (contact.lastName.toLocaleLowerCase().includes(query)) count++
-
-      if (count > 0) {
-        matchCount += count
-        count
-        matchCount
-
-        matchMap[query] = (matchMap[query] || 0) + count // Увеличиваем счетчик для слова
-        bestMatch
-        // Проверяем, является ли текущее слово наиболее часто встречающимся
-        console.log(!bestMatch)
-        console.log(matchMap[query])
-        console.log(matchMap[bestMatch])
-        if (!bestMatch || matchMap[query] > matchMap[bestMatch]) {
-          bestMatch
-          matchMap
-          bestMatch = query
-          matchCount
-          bestMatch
-        }
+      if (
+        contact.phone.toLocaleLowerCase().includes(query) ||
+        contact.firstName.toLocaleLowerCase().includes(query) ||
+        contact.lastName.toLocaleLowerCase().includes(query)
+      ) {
+        matchCount++
       }
     })
-    if (matchCount > 0) {
-      findedContacts.push(bestMatch)
-    }
+
+    matchCount
+    return { contact, matchCount }
   })
 
-  return findedContacts
+  const maxMatches = Math.max(...contactMatches.map(item => item.matchCount))
+
+  return contactMatches
+    .filter(item => item.matchCount === maxMatches && item.matchCount > 0)
+    .map(item => item.contact)
 }
 
 const results = findContactsByQuery('   ирина   иринова   3  ')
